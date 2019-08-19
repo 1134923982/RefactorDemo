0faf6a3fff11de1aa838cdf91c3ae805b713557f
1134923982 <1134923982@qq.com>
2019-08-19 18:38:49 +0800

    [create test should_return_quality_decrement_0_and_sellIn_decrement_1_when_given_items_name_is_begin_with_backstage_and_its_sellIn_is_5_quality_is_10]

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -74,7 +74,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void should_return_quality_decrement_0_and_sellIn_decrement_1_when_given_items_name_is_aged_brie_and_its_sellIn_is_2_quality_is_2() throws Exception {
+    public void should_return_quality_add_1_and_sellIn_decrement_1_when_given_items_name_is_aged_brie_and_its_sellIn_is_2_quality_is_2() throws Exception {
         //given
         Item item1 = new Item("Aged Brie",2,2);
         Item[] items = {item1};
@@ -90,7 +90,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void should_return_quality_decrement_0_and_sellIn_decrement_1_when_given_items_name_is_aged_brie_and_its_sellIn_is_0_quality_is_1() throws Exception {
+    public void should_return_quality_add_2_and_sellIn_decrement_2_when_given_items_name_is_aged_brie_and_its_sellIn_is_0_quality_is_1() throws Exception {
         //given
         Item item1 = new Item("Aged Brie",0,1);
         Item[] items = {item1};
@@ -106,7 +106,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void should_return_quality_decrement_0_and_sellIn_decrement_1_when_given_items_name_is_aged_brie_and_its_sellIn_is_0_quality_is_49() throws Exception {
+    public void should_return_quality_add_1_and_sellIn_decrement_1_when_given_items_name_is_aged_brie_and_its_sellIn_is_0_quality_is_49() throws Exception {
         //given
         Item item1 = new Item("Aged Brie",0,49);
         Item[] items = {item1};
@@ -119,5 +119,20 @@ public class GildedRoseTest {
         //then
         Assert.assertEquals(50 ,actualItems[0].quality);
         Assert.assertEquals(-1 ,actualItems[0].sellIn);
+    }
+    @Test
+    public void should_return_quality_decrement_0_and_sellIn_decrement_1_when_given_items_name_is_begin_with_backstage_and_its_sellIn_is_5_quality_is_10() throws Exception {
+        //given
+        Item item1 = new Item("Backstage passes to a TAFKAL80ETC concert",5,10);
+        Item[] items = {item1};
+        GildedRose gildedRose = new GildedRose(items);
+
+        //when
+        gildedRose.updateQuality();
+        Item[] actualItems = gildedRose.getItems();
+
+        //then
+        Assert.assertEquals(13 ,actualItems[0].quality);
+        Assert.assertEquals(4 ,actualItems[0].sellIn);
     }
 }
