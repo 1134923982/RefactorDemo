1bfbf83e40d6068830d7165848a23399fa015189
1134923982 <1134923982@qq.com>
2019-08-19 17:58:37 +0800

    [create test should_return_quality_decrement_0_and_sellIn_decrement_1_when_given_items_name_not_is_special_name_and_its_sellIn_is_0_quality_is_0]

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -56,4 +56,20 @@ public class GildedRoseTest {
         Assert.assertEquals(0 ,actualItems[0].quality);
         Assert.assertEquals(-1 ,actualItems[0].sellIn);
     }
+
+    @Test
+    public void should_return_quality_decrement_0_and_sellIn_decrement_1_when_given_items_name_not_is_special_name_and_its_sellIn_is_0_quality_is_0() throws Exception {
+        //given
+        Item item1 = new Item("item1",0,0);
+        Item[] items = {item1};
+        GildedRose gildedRose = new GildedRose(items);
+
+        //when
+        gildedRose.updateQuality();
+        Item[] actualItems = gildedRose.getItems();
+
+        //then
+        Assert.assertEquals(0 ,actualItems[0].quality);
+        Assert.assertEquals(-1 ,actualItems[0].sellIn);
+    }
 }
