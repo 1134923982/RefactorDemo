b8aceeb157531b4bbbc3247ffc62fe6bf7854716
1134923982 <1134923982@qq.com>
2019-08-19 18:08:17 +0800

    [create test should_return_quality_decrement_0_and_sellIn_decrement_1_when_given_items_name_is_aged_brie_and_its_sellIn_is_0_quality_is_1] [create test should_return_quality_decrement_0_and_sellIn_decrement_1_when_given_items_name_is_aged_brie_and_its_sellIn_is_0_quality_is_49]

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -88,4 +88,36 @@ public class GildedRoseTest {
         Assert.assertEquals(3 ,actualItems[0].quality);
         Assert.assertEquals(1 ,actualItems[0].sellIn);
     }
+
+    @Test
+    public void should_return_quality_decrement_0_and_sellIn_decrement_1_when_given_items_name_is_aged_brie_and_its_sellIn_is_0_quality_is_1() throws Exception {
+        //given
+        Item item1 = new Item("Aged Brie",0,1);
+        Item[] items = {item1};
+        GildedRose gildedRose = new GildedRose(items);
+
+        //when
+        gildedRose.updateQuality();
+        Item[] actualItems = gildedRose.getItems();
+
+        //then
+        Assert.assertEquals(3 ,actualItems[0].quality);
+        Assert.assertEquals(-1 ,actualItems[0].sellIn);
+    }
+
+    @Test
+    public void should_return_quality_decrement_0_and_sellIn_decrement_1_when_given_items_name_is_aged_brie_and_its_sellIn_is_0_quality_is_49() throws Exception {
+        //given
+        Item item1 = new Item("Aged Brie",0,49);
+        Item[] items = {item1};
+        GildedRose gildedRose = new GildedRose(items);
+
+        //when
+        gildedRose.updateQuality();
+        Item[] actualItems = gildedRose.getItems();
+
+        //then
+        Assert.assertEquals(50 ,actualItems[0].quality);
+        Assert.assertEquals(-1 ,actualItems[0].sellIn);
+    }
 }
