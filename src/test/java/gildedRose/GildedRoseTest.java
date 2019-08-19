dc1e7c82c9e3f4366a123988b7f6a6dd7649b547
1134923982 <1134923982@qq.com>
2019-08-19 18:46:53 +0800

    [test][create test should_return_quality_is_0_and_sellIn_decrement_1_when_given_items_name_is_begin_with_backstage_and_its_sellIn_is_0_quality_is_less_than_0]

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -183,4 +183,20 @@ public class GildedRoseTest {
         Assert.assertEquals(0 ,actualItems[0].quality);
         Assert.assertEquals(-1 ,actualItems[0].sellIn);
     }
+
+    @Test
+    public void should_return_quality_is_0_and_sellIn_decrement_1_when_given_items_name_is_begin_with_backstage_and_its_sellIn_is_0_quality_is_less_than_0() throws Exception {
+        //given
+        Item item1 = new Item("Backstage passes to a TAFKAL80ETC concert",0,-1);
+        Item[] items = {item1};
+        GildedRose gildedRose = new GildedRose(items);
+
+        //when
+        gildedRose.updateQuality();
+        Item[] actualItems = gildedRose.getItems();
+
+        //then
+        Assert.assertEquals(0 ,actualItems[0].quality);
+        Assert.assertEquals(-1 ,actualItems[0].sellIn);
+    }
 }
